4a3cc6e3521f0145da689f6c0f78e9f53bd53555
dominique39 <440000323@gapps.yrdsb.ca>
2023-11-23 12:56:08 -0500

    relocate

--- java/Elephant.java
+++ java/Elephant.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class elephant extends Actor
+public class Elephant extends Actor
 {
     /**
      * Act - do whatever the elephant wants to do. This method is called whenever
