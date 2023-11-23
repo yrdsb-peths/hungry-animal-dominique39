10d6a1f085f774be1a51e6a9d2607790531eb914
dominique39 <440000323@gapps.yrdsb.ca>
2023-11-23 13:03:23 -0500

    Update MyWorld.java

--- java/MyWorld.java
+++ java/MyWorld.java
@@ -1,23 +1,17 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;
 
 /**
  * Write a description of class MyWorld here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Dominique
+ * @version v1.0
  */
 public class MyWorld extends World
 {
-
-    /**
-     * Constructor for objects of class MyWorld.
-     * 
-     */
-    public MyWorld()
-    {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+    public MyWorld(){    
         super(600, 400, 1);
 
         Elephant elephant = new Elephant();
+        addObject(elephant,300,300);
     }
 }
