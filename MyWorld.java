53a2ecbef558d62f43386b9b00ffc46b2bfe9ad5
dominique39 <440000323@gapps.yrdsb.ca>
2023-11-25 00:56:43 -0500

    Added Scores

--- MyWorld.java
+++ MyWorld.java
@@ -11,29 +11,32 @@ public class MyWorld extends World
 {
     public int score = 0;
     public boolean gameover = false;
+    
     Elephant elephant = new Elephant();
+    Bomb bomb = new Bomb();
+    Mushroom mushroom = new Mushroom();
     
     public MyWorld(){    
         super(600, 400, 1, true);
+        setBackground("bg/Ocean_1/4.png");
         Apple.clearApples();
         Apple apple = new Apple();
         Apple apple2 = new Apple();
         Apple apple3 = new Apple();
         Apple apple4 = new Apple();
         GrnApple gapple = new GrnApple();
-        Bomb bomb = new Bomb();
-        Mushroom mushroom = new Mushroom();
         
-        addObject(mushroom, 300, 0);
         addObject(elephant,300,300);
         for(int i = 0; i < Apple.getNum();i++){
             addObject(Apple.getApple(i),Greenfoot.getRandomNumber(600),Greenfoot.getRandomNumber(150));
         }
         
+        showText("Score: "+score, 300, 20);
     }
     
     public void addscore(){
         score++;
+        showText("Score: "+score, 300, 20);
     }
     
     public void attractObjs(){
