c630754f64138b7f87f238d4014d5d0a2543d74b
dominique39 <440000323@gapps.yrdsb.ca>
2023-12-07 01:26:15 -0500

    Added intro and instructions

--- Title.java
+++ Title.java
@@ -5,6 +5,7 @@ public class Title extends World
     public static boolean demo;
     Label textLabel;
     Label instructionLabel;
+    Label introLabel;
     Elephant character;
     Apple food;
 
@@ -16,7 +17,16 @@ public class Title extends World
         textLabel.setFillColor(Color.RED);
         addObject(textLabel, 600, 200);
 
-        instructionLabel = new Label("Press space to Start", 30);
+        introLabel = new Label("The elephant is hungry! Eat as much food as you can! \n" +
+                                "Green apples taste better than red apples! Eating mushroom feels powerful. \n" +
+                                "However, beware of falling bombs!", 30);
+        introLabel.setFillColor(Color.BLACK);
+        addObject(introLabel, 600, 400);
+        
+        instructionLabel = new Label("Use 'a' or '<-' to move left; \n" + 
+                                    "Use 'd' or '->' to move right; \n" + 
+                                    "Use shift to dash. \n" + 
+                                    "Press space to Start", 30);
         instructionLabel.setFillColor(Color.ORANGE);
         instructionLabel.setLineColor(Color.YELLOW);
         addObject(instructionLabel, 600, 600);
@@ -41,6 +51,7 @@ public class Title extends World
     public void toGame(){
         textLabel = null;
         instructionLabel = null;
+        introLabel = null;
         character = null;
         food = null;
 
